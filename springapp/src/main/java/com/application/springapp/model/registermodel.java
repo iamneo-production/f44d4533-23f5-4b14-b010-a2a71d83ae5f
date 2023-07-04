54cdd0f45798ab518f191f7bce2d59514e62f7a6
rahular2313 <rahularuchamy@gmail.com>
2023-07-04 09:03:25 +0000

    Revert "Merge pull request #33 from iamneo-production/Acservice-rahular2313"
    This reverts commit 9bf9e82165b2fa993b4bf0fbfa92f5c835df36d0, reversing
    changes made to 049f0c8b30c1eb164ad6a51c18abe2d23a3cb21f.

--- springapp/src/main/java/com/application/springapp/model/registermodel.java
+++ springapp/src/main/java/com/application/springapp/model/registermodel.java
@@ -1,4 +1,4 @@
-package com.examly.springapp.model;
+package com.application.springapp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- springapp/src/main/java/com/application/springapp/registercontroller/register.java
+++ springapp/src/main/java/com/application/springapp/registercontroller/register.java
@@ -1,4 +1,4 @@
-package com.examly.springapp.controller;
+package com.application.springapp.registercontroller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.examly.springapp.model.registermodel;
-import com.examly.springapp.service.RegisterService;
+import com.application.springapp.model.registermodel;
+import com.application.springapp.service.RegisterService;
 
 
 @RestController
