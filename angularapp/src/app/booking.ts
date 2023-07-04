6cd771c60a1ad94b4222e0faeb2235d164d167e3
gowrisankar85080 <gowrisankar85080@gmail.com>
2023-07-04 13:46:46 +0000

    las

---
+++ angularapp/src/app/booking.ts
@@ -0,0 +1,9 @@
+export class Booking {
+    id!:number;
+    deviceType!:string;
+    deviceBrand!:string;
+    deviceModel!:string;
+    problem!:string;
+    date!:string;
+    timeSlot!:string;
+}
