6cfccb8f5749e2a47a54705242df0c3eae7ee090
rahular2313 <rahularuchamy@gmail.com>
2023-07-04 09:11:52 +0000

    commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,7 +1,8 @@
 package com.examly.springapp;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Configuration
 @SpringBootApplication
 public class SpringappApplication {
 
