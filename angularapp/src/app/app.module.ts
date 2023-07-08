161eeb9abc494dbcfa4ef66305894f4890d96030
rahular2313 <rahularuchamy@gmail.com>
2023-07-08 05:34:54 +0000

    commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+npm import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
