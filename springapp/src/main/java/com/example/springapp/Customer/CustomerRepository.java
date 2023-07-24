48f967e02e9d81277569be3a81e7a74ff6a05e96
furyflow <arvindans22@gmail.com>
2023-07-24 08:34:14 +0000

    first

--- springapp/src/main/java/com/example/springapp/Customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapp.Customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.io.Serializable;
-
-public interface CustomerRepository extends JpaRepository<Customer, Serializable> {
-
-}
--- springapp/src/main/java/com/example/springapp/Customer/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapp.Customer;
-
-public interface CustomerService {
-    public String upsert(Customer customer);
-
-    public Customer getById(String id);
-}
